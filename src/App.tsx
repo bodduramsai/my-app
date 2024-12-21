fbf1899e49b744947992a2c0a3b7290052e1b619
bodduramsai <22bk1a7214@stpetershyd.com>
2024-12-21 12:27:20 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+import Photos from "./Components/Photos";
+
+export default function App() {
+  return (
+    <div>
+      <Photos/>
+    </div>
+  );
+}
